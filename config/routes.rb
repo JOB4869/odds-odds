aed8dd5117d5d04f40a70f23660ba16c39f23332
JOB4869 <kittitatjob@gmail.com>
2025-03-20 11:43:16 +0700

    Add user registration functionality with email validation and password security

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
+  get "sign_up" => "registrations#new", as: :sign_up
+  post "sign_up" => "registrations#create"
+  get "sign_in" => "sessions#new", as: :sign_in
+  post "sign_in" => "sessions#create"
+  delete "sign_out" => "sessions#destroy", as: :sign_out
 
   root to: "main#index"
 end
