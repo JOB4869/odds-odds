4c22c65892d6264cbd09e9a3f1195147c9c79ba0
JOB4869 <kittitatjob@gmail.com>
2025-03-20 16:16:46 +0700

    Implement user registration with enhanced validations and error handling

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,20 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_session_path, notice: "สร้างบัญชีผู้ใช้เรียบร้อยแล้ว"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "ต้องเป็นที่อยู่อีเมลที่ถูกต้อง" }
+  validates :email, presence: { message: "กรุณากรอกอีเมล" },
+                   uniqueness: { message: "อีเมลนี้ถูกใช้งานแล้ว" },
+                   format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "รูปแบบอีเมลไม่ถูกต้อง" }
+
+  validates :password, presence: { message: "กรุณากรอกรหัสผ่าน" },
+                      length: { minimum: 8, maximum: 16, message: "รหัสผ่านต้องมีความยาวระหว่าง 8-16 ตัวอักษร" },
+                      format: {
+                        with: /\A(?=.*[a-zA-Z])(?=.*\d)(?=.*[!"#$%&'()*+,-.\/:;<=>?@[\\]^_`{|}~])[A-Za-z\d!"#$%&'()*+,-.\/:;<=>?@[\\]^_`{|}~]{8,16}\z/,
+                        message: "รหัสผ่านต้องประกอบด้วยตัวอักษร ตัวเลข และสัญลักษณ์พิเศษ"
+                      }
 end
