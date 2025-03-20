e0c7cdff995d38dcb7fbd4e68c44ac8d22fee25e
JOB4869 <kittitatjob@gmail.com>
2025-03-20 14:53:02 +0700

    Add password toggle functionality and enhance registration form layout

---
+++ app/javascript/controllers/password_toggle_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["checkbox", "password"]
+
+  connect() {
+    this.togglePassword = this.togglePassword.bind(this)
+  }
+
+  togglePassword(event) {
+    const isChecked = event.target.checked
+    this.passwordTargets.forEach(field => {
+      field.type = isChecked ? 'text' : 'password'
+    })
+  }
+} 
