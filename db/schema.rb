04db7df7e585d2c1b88946de7464dc1f6ed68b83
JOB4869 <kittitatjob@gmail.com>
2025-03-26 17:01:49 +0700

    Add password reset token to users; update view styles and enable asset compilation in development

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_25_095036) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_26_094008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -63,6 +63,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_25_095036) do
     t.string "phone"
     t.text "address"
     t.string "prompt_pay"
+    t.string "password_reset_token"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
