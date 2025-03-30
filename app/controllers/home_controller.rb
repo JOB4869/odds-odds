08086178620580c807dc2b1232e365d569351fd5
JOB4869 <kittitatjob@gmail.com>
2025-03-30 19:31:17 +0700

    Add cart initialization in HomeController; update footer to display item count in cart; enhance user index view with navigation link to home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
+  before_action :set_cart
+
   def index
     @user = Current.user
     @products = Product.order(created_at: :desc)
+  end
+
+  private
+
+  def set_cart
+    @current_cart = Cart.find_or_create_by(user: Current.user)
   end
 end
