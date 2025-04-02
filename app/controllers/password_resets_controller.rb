76c73c2a581453b6b052d7e37f4602204a53fef5
JOB4869 <kittitatjob@gmail.com>
2025-04-02 22:12:12 +0700

    Fix password validation error handling in password resets controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -44,7 +44,7 @@ class PasswordResetsController < ApplicationController
   def valid_password?
     password = params[:user][:password]
     unless password.match?(/\A(?=.*[a-zA-Z])(?=.*\d)(?=.*[!"#$%&'()*+,-.\/:;<=>?@[\\]^_`{|}~])[A-Za-z\d!"#$%&'()*+,-.\/:;<=>?@[\\]^_`{|}~]{8,16}\z/)
-      Current.user.errors.add(:password, "รหัสผ่านต้องมีความยาวระหว่าง 8-16 ตัวอักษร และประกอบด้วยตัวอักษร ตัวเลข และสัญลักษณ์พิเศษ")
+      @user.errors.add(:password, "รหัสผ่านต้องมีความยาวระหว่าง 8-16 ตัวอักษร และประกอบด้วยตัวอักษร ตัวเลข และสัญลักษณ์พิเศษ")
       return false
     end
     true
