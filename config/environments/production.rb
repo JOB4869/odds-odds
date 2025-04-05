a5566bcfc82aaa9b041c8104c464a4011fca6bd6
JOB4869 <kittitatjob@gmail.com>
2025-04-06 05:49:52 +0700

    Remove hardcoded database name from production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,9 +79,6 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
-  config.active_record.database = odds_odds_db
-
-
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
