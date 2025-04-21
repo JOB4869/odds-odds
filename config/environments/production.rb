493dcfd2472e59ab357cba65fcead54c39b4ecaf
JOB4869 <kittitatjob@gmail.com>
2025-04-21 19:00:45 +0700

    Improved action_mailer settings in production.rb by enabling error reporting on email delivery and email sending.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,12 +55,13 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "odds-odds-shop.onrender.com" }
 
-  # Specify outgoing SMTP server
+  # Specify outgoing SMTP server.
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
