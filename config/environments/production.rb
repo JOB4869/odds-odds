218e1accb9a8bc4697e3000feb31ef82c4cae601
JOB4869 <kittitatjob@gmail.com>
2025-04-21 18:46:41 +0700

    Improved the settings in production.rb by changing the cache storage to memory_store and adjusting the settings for Active Job to async, including turning off error reporting for email delivery.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,9 +22,7 @@ Rails.application.configure do
   # config.asset_host = "http://assets.example.com"
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  # config.active_storage.service = :local
   config.active_storage.service = :cloudinary
-
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
@@ -49,20 +47,20 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :solid_cache_store
+  config.cache_store = :memory_store
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
-  config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  config.active_job.queue_adapter = :async
+  # config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "odds-odds-shop.onrender.com" }
 
-  # Specify outgoing SMTP server.
+  # Specify outgoing SMTP server
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
@@ -73,8 +71,6 @@ Rails.application.configure do
     authentication: "plain",
     enable_starttls_auto: true
   }
-
-  config.action_mailer.perform_deliveries = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -94,7 +90,4 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.assets.compile = false  # เพื่อให้โหลดไฟล์จาก precompiled assets
-  config.assets.digest = true   # เพื่อให้แน่ใจว่าไฟล์มี hash เพื่อ cache ได้
-  config.assets.precompile += %w[ tailwind-18293f78.css ]
 end
